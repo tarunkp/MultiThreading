fa615513c229ed4bc8b00a981b8ed3f3768eccfe
tarun <parihartarun@gmail.com>
2019-08-17 00:28:10 -0700

    Add script to compile and run the tests

---
+++ Using_CPP11/run.sh
@@ -0,0 +1,19 @@
+#
+
+#
+g++ -Wall -std=c++14 -pthread 1_creatingThreads.cpp -o creatingThreads
+./creatingThreads
+
+g++ -Wall -std=c++11 -pthread 2_passingRefToThreads.cpp -o passingRefToThreads
+./passingRefToThreads
+
+g++ -Wall -std=c++11 -pthread 3_usingMutex.cpp -o usingMutex
+./usingMutex
+
+g++ -Wall -std=c++11 -pthread 4_usingAtomic.cpp -o usingAtomic
+./usingAtomic
+
+g++ -Wall -std=c++11 -pthread 5_usingConditionVar.cpp -o usingCondVar
+./usingCondVar
+
+
